78e5bbe745096ea62fbb4b3f671faa2473bcb308
Dylan Scott <drrscott@hotmail.com>
2016-05-25 09:00:47 -0500

    nginx fix
    nginx fix

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,37 +1,17 @@
 class nginx {
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
+  file { 'nginx rpm' :
+    ensure   => file,
+    path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
   }
 
-  package { 'nginx':
-    ensure  => 'present',
+  package { 'nginx' :
+    ensure   => '1.6.2-1.el7.centos.ngx',
+    source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    provider => rpm,
+    require  => File['nginx rpm'],
   }
-
-  file { [ '/var/www', '/etc/nginx/conf.d' ]:
-     ensure => directory,
-  }
-
-  file { '/var/www/index.html':
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
-  }
-
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    notify => Service['nginx'],
-    require => Package['nginx'],
-  }
-
+}
   service { 'nginx':
      ensure => running,
      enable => true,
